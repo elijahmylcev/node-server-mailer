0a8148e4ce4d3721638228aa946b52e3bb971978
Elijah <elijahmylcev@gmail.com>
2021-11-01 21:29:43 +0300

    working server --> Bootstrap form --> primitiv validation

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express()
 
 const PORT = 3001;
 
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/form', (req,res)=> {
+    console.log(req.body);
+})
+
 app.get('/form', (req, res) => {
-    res.send('Hello Server')
+    res.sendFile(__dirname + '/form.html')
 })
 
 app.listen(PORT, () => {
-    console.log(`server start at http://localhost:${PORT}/form`);
+    console.log(`server start --> at http://localhost:${PORT}/form`);
 })

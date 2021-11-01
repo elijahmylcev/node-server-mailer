a799bb105344e4532cfedb49b2f09fe0301a9ee1
Elijah <elijahmylcev@gmail.com>
2021-11-01 22:15:18 +0300

    upgrade index.js --> working server model

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const bodyParser = require('body-parser');
 const app = express()
 
 const PORT = 3001;
+let user = undefined;
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/form', (req,res)=> {
+
+    if (!req.body.email || !req.body.text) return res.status(400);
+    
     console.log(req.body);
+    user = req.body;
+    res.redirect('/form')
 })
 
 app.get('/form', (req, res) => {
-    res.sendFile(__dirname + '/form.html')
+    if (typeof user !== 'object') return res.sendFile(__dirname + '/form.html');
+
+    res.send(`Ваше сообщение отправлено на email: ${user.email}`);
+
+    user = undefined;
 })
 
 app.listen(PORT, () => {
